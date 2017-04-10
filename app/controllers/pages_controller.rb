f633c47546493009041e977447f28d15197c3abc
Margarita Bartholomew <margarita.veracruz@gmail.com>
2017-04-10 13:16:23 -0400

    creating a different header for each page, using instance variables

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,13 @@
 class PagesController < ApplicationController
   def welcome
+    @header = "Welcome Header"
   end
 
   def about
+    @header = "About Header"
   end
 
   def contest
+    @header = "Contest Header"
   end
 end
