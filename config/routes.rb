3035764b963b3422ac9172a10024e80c85ee854f
Margarita Bartholomew <margarita.veracruz@gmail.com>
2017-04-10 13:13:08 -0400

    making the about page work for /about

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/' => 'pages#welcome'
   get '/welcome' => 'pages#welcome'
-
+  get '/about' => 'pages#about'
 
 end
